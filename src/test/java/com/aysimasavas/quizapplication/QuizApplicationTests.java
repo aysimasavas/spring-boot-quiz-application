3df27f9ee67f2caec96065a4b80c9b0d76fbf4b1
aysimasavas <aysimasavas1@gmail.com>
2022-04-23 12:53:20 +0300

    init commit

---
+++ src/test/java/com/aysimasavas/quizapplication/QuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aysimasavas.quizapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
